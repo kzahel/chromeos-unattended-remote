eed3d80133c9921b0445a48a485905012212d845
Kyle Graehl <kyle@graehlarts.com>
2019-07-28 21:25:26 -0700

    add rpc server and fake touchscreen

---
+++ rpcserver/faketouchscreen.py
@@ -0,0 +1,58 @@
+import time
+import random
+from evdev import UInput, AbsInfo
+from evdev.ecodes import EV_ABS, EV_KEY, \
+    ABS_MT_TRACKING_ID, \
+    ABS_MT_POSITION_X, \
+    ABS_MT_POSITION_Y, \
+    BTN_TOUCH, \
+    ABS_X, \
+    ABS_Y, \
+    ABS_PRESSURE, \
+    ABS_MT_PRESSURE
+TMIN = 0.01
+XMAX = 1920
+YMAX = 1080
+TRACKIDMAX = 65535
+RES = 10
+cap = {
+    EV_KEY: [BTN_TOUCH],
+    EV_ABS: [
+        (ABS_X, AbsInfo(value=0, min=0, max=XMAX,
+                        fuzz=0, flat=0, resolution=RES)),
+        (ABS_Y, AbsInfo(0, 0, YMAX, 0, 0, RES)),
+        (ABS_PRESSURE, AbsInfo(0, 0, 255, 0, 0, 0)),
+        (ABS_MT_TRACKING_ID, AbsInfo(0, 0, TRACKIDMAX, 0, 0, 0)),
+        (ABS_MT_PRESSURE, AbsInfo(0, 0, 255, 0, 0, 0)),
+        (ABS_MT_POSITION_X, AbsInfo(0, 0, XMAX, 0, 0, RES)),
+        (ABS_MT_POSITION_Y, AbsInfo(0, 0, YMAX, 0, 0, RES))
+    ]
+}
+
+class FakeTouchscreen:
+    def __init__(self):
+        self.d = UInput(cap, name='Fake Touchscreen', version=0x3)
+
+    def down(self,x,y):
+        trackid = int(random.random() * TRACKIDMAX)
+        self.d.write(EV_ABS, ABS_MT_TRACKING_ID, trackid)
+        self.d.write(EV_ABS, ABS_MT_POSITION_X, x)
+        self.d.write(EV_ABS, ABS_MT_POSITION_Y, y)
+        self.d.write(EV_ABS, ABS_X, x)
+        self.d.write(EV_ABS, ABS_Y, y)
+        self.d.write(EV_KEY, BTN_TOUCH, 1)
+        self.d.write(EV_ABS, ABS_PRESSURE, 255)
+        self.d.write(EV_ABS, ABS_MT_PRESSURE, 255)
+        self.d.syn()
+
+    def up(self):
+        self.d.write(EV_ABS, ABS_MT_TRACKING_ID, -1)
+        self.d.write(EV_KEY, BTN_TOUCH, 0)
+        self.d.write(EV_ABS, ABS_PRESSURE, 0)
+        self.d.write(EV_ABS, ABS_MT_PRESSURE, 0)
+        self.d.syn()
+
+    def touch(self,x,y):
+        self.down(x,y)
+        time.sleep(TMIN)
+        self.up()
---
+++ rpcserver/server.py
@@ -0,0 +1,102 @@
+import SimpleHTTPServer
+import SocketServer
+import base64
+import json
+import os
+import io
+
+import pdb
+
+from screenshot.gbm import crtcScreenshot
+from faketouchscreen import FakeTouchscreen
+touchscreen = FakeTouchscreen()
+
+DEFAULT_PORT = 8000
+def initialize_settings():
+    if os.path.exists('settings.json'):
+        with open('settings.json') as fo:
+            settings = json.loads(fo.read())
+    else:
+        password = base64.b64encode(os.urandom(40))
+        settings = dict(password=password, port=DEFAULT_PORT)
+        with open('settings.json','w') as fo:
+            fo.write(json.dumps(settings,indent=2))
+    return settings
+
+settings = initialize_settings()
+print 'listening on 127.0.0.1:%s' % settings['port']
+print 'rpc password is:', settings['password']
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_HEAD(self):
+        print 'head',self.path
+        self.send_response(200,'ok')
+
+    def authenticate(self):
+        if 'authorization' in self.headers:
+            s = self.headers['authorization'].split(' ')[1]
+            username, password = base64.b64decode(s).split(':')
+            if password != settings['password']:
+                return self.unauthorized()
+        else:
+            self.unauthorized()
+            
+    def unauthorized(self):
+        print '403 unauthorized'
+        self.send_response(403)
+        self.end_headers()
+        self.wfile.write('unauthorized')
+
+    def do_click(self, x, y):
+        touchscreen.touch(x,y)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('touched.')
+        
+    def do_screenshot(self):
+        screen_num=0
+        image = crtcScreenshot(screen_num)
+
+        imgByteArr = io.BytesIO()
+        image.save(imgByteArr, format='PNG')
+        imgByteArr = imgByteArr.getvalue()
+
+        self.send_response(200)
+        self.end_headers()
+
+        self.wfile.write(imgByteArr)
+
+        
+    def do_icon(self):
+        with open('icon16.png', 'rb') as fo:
+            data = fo.read()
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(data)
+        
+    def do_GET(self):
+        self.authenticate()
+        if self.path == '/icon':
+            return self.do_icon()
+        if self.path == '/screenshot':
+            return self.do_screenshot()
+        if self.path.startswith('/click'):
+            params = dict( kv.split('=') for kv in self.path.split('?')[1].split('&') )
+            for k,v in params.items():
+                params[k] = int(v)
+            print 'parsed params', params
+            return self.do_click(params['x'], params['y'])
+        print 'get',self.path
+        body = 'OKOKOK'
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(body)
+
+httpd = SocketServer.TCPServer(('127.0.0.1', settings['port']), Handler, bind_and_activate=False)
+httpd.allow_reuse_address = True
+httpd.server_bind()
+httpd.server_activate()
+httpd.serve_forever()
